058b8bcf2177d603c36bb7d300ffa352dc3a39aa
longdahai <longdahai3@163.com>
2020-06-20 17:53:36 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import {Route,Switch,BrowserRouter as Router} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>new computed</h1>
+    </div>
+  );
+}
+
+export default App;
